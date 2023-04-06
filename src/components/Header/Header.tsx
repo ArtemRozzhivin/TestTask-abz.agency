1720d9b02047ad09dcc487376f25a0f4ad76cdef
ArtemRozzhivin <artiomrozzhyvin@gmail.com>
2023-04-06 19:44:10 +0300

    create header / main component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Button from '../../ui/Button/Button';
+
+import logo from '../../assets/image/Logo.svg';
+
+import './Header.scss';
+
+const Header: React.FC = () => {
+  return (
+    <div className="header">
+      <div className="header__menu">
+        <div className="header__logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="header__buttons">
+          <div className="header__users">
+            <Button>Users</Button>
+          </div>
+          <div className="header__sign">
+            <Button>Sign up</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/ui/Button/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Button.scss';
+
+interface ButtonType {
+  children: string;
+}
+
+const Button: React.FC<ButtonType> = ({ children }) => {
+  return (
+    <button className="button" type="button">
+      <div className="button__text">{children}</div>
+    </button>
+  );
+};
+
+export default Button;
